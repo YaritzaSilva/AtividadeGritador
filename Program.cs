c4f7da9296c998e0e9060cc4a38b6742d698fc6c
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-07 11:17:26 -0300

    Atividade dia 5

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AtividadeGritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoDigitado;
+            Console.Write("Digite algo:");
+            textoDigitado = Console.ReadLine();
+
+            Console.WriteLine($"Você digitou:{textoDigitado.ToUpper()}");
+        }
+    }
+}
